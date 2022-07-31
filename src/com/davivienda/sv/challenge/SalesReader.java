9b68659ca1434760222efc5a5218ec04235a6b1c
s2rgi0 <tlakolu@gmail.com>
2022-07-31 11:58:18 -0600

    commit javaChallenge

--- src/com/davivienda/sv/challenge/SalesReader.java
+++ src/com/davivienda/sv/challenge/SalesReader.java
@@ -13,7 +13,24 @@ public class SalesReader {
 	public static String FILE_NAME = "sales.csv";
 	
 	public List<SalesBean> getSales(InputStream stream) {
-		//TODO: implementar
-		return null;
+		String linea = "";
+		List<SalesBean> sales = new ArrayList<SalesBean>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		
+		 try {
+			reader.readLine();
+			while ((linea = reader.readLine()) != null) {
+				SalesBean sale = new SalesBean(linea);
+				//System.out.println("Sales Bean : "+sale.toString());
+				sales.add(sale);
+			 }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return sales;
 	}
+	
 }
