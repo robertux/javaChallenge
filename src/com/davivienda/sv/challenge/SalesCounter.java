3f10052c2cde84811b81210f2401db82dc00b072
waltex23 <walter.galdamez@gmail.com>
2022-08-01 20:26:26 -0600

    Update SalesCounter.java

--- src/com/davivienda/sv/challenge/SalesCounter.java
+++ src/com/davivienda/sv/challenge/SalesCounter.java
@@ -1,17 +1,15 @@
 package com.davivienda.sv.challenge;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class SalesCounter {
 	private List<SalesBean> sales;
-
+	
 	public SalesCounter() {
 		this.sales = new ArrayList<SalesBean>();
 	}
@@ -20,22 +18,47 @@ public class SalesCounter {
 		this.sales = sales;
 	}
 	
-	public Date getOldestDate() {
-		//TODO: Implementar
-		return null;
+	public Date getOldestDate() throws ParseException {
+		//TODO: Implementar fecha de la venta más antigua
+		
+		Comparator<SalesBean> saleDateComparator = Comparator.comparing(SalesBean::getOrderDate);
+		SalesBean saleOldestDate = getSales().stream().min(saleDateComparator).get();
+		
+		return saleOldestDate.getOrderDate();
 	}
 	
 	public String getMostExpensiveItem() {
-		//TODO: Implementar
-		return null;
+		//TODO: Implementar nombre del item más caro en las ventas
+		
+		Comparator<SalesBean> expensiveItemComparator = Comparator.comparing(SalesBean::getUnitPrice);
+		SalesBean expensiveItem = getSales().stream().max(expensiveItemComparator).get();
+	
+		return expensiveItem.getItemType();
 	}
 	
 	public String getCountryWithMostSales() {
-		//TODO: Implementar
-		return null;
+		//TODO: Implementar país que haya tenido más ventas
+		  String country= ""; 
+		  BigDecimal sumaTP = new BigDecimal(0);
+		  BigDecimal sumaTS= new BigDecimal(0);
+		  int i; int j=1; 
+		  for(i= 1; i< sales.size(); i++) { 
+			  for(j= 2; j < (sales.size()-1); j++) {
+				  if(sales.get(i).getCountry().equals(sales.get(j).getCountry())){ 
+					  sumaTP = sumaTP.add(sales.get(i).getTotalRevenue());
+					  }
+				  }
+			  int resul= 0;
+			  resul= sumaTP.compareTo(sumaTS);
+			  if (resul == 1){			  
+				  sumaTS = sumaTS.add(sumaTP);
+				  country= sales.get(i).getCountry();
+			  }	
+		 }
+		return country;
 	}
 	
-	public List<SalesBean> getSales() {
+	public List<SalesBean> getSales() {		
 		return sales;
 	}
 
